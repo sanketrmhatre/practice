5240260043b35538d0f284f0e72b9758c094f6d4
sanketrmhatre <154064403+sanketrmhatre@users.noreply.github.com>
2024-10-05 12:13:09 +0530

    Add files via upload

---
+++ practice/operation.js
@@ -0,0 +1,14 @@
+//operations //
+
+let str1 = "Sanket";
+let str2 = " Mhatre";
+
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+2);
+
+
+
+
---
+++ practice/string.js
@@ -0,0 +1,7 @@
+const name ='sanket'
+const repoCount = 50
+
+console.log(name + repoCount + " Value");
+
+
+
